d60fcc9d59f82cbf69a2f5be41fd35ce2baa360d
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-10-13 17:06:51 +0000

    add comments section

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   
   root 'welcome#index'
 
